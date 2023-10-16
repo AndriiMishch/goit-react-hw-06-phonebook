56bef10bde79f518ec8818fa612582299172bb7f
AndriiMishch <anderito02@gmail.com>
2023-10-16 21:21:40 +0200

    Update index.jsx

--- src/components/UserList/UserListItem/index.jsx
+++ src/components/UserList/UserListItem/index.jsx
@@ -1,12 +1,20 @@
-import { Button } from 'components/BaseStyles.styled';
+import { useDispatch } from 'react-redux';
+import { deleteContact } from 'redux/contactsSlice';
 
-export default function UserListItem({ id, name, number, onDelete }) {
+export default function ContactListItem({ contact }) {
+  const dispatch = useDispatch();
+
+  const handleDelete = id => {
+    dispatch(deleteContact(id));
+  };
+
   return (
-    <li>
-      {name}: {number}
-      <Button $primary={true} onClick={() => onDelete(id)}>
+    <>
+      <span>{contact.name}</span>
+      <span>{contact.tel}</span>
+      <button type="button" onClick={() => handleDelete(contact.id)}>
         Delete
-      </Button>
-    </li>
+      </button>
+    </>
   );
 }
