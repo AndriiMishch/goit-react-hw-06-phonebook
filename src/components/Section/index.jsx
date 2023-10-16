fe396676e8e6a61f9414a659affce34150766e37
AndriiMishch <anderito02@gmail.com>
2023-10-16 21:21:13 +0200

    Update index.jsx

--- src/components/Section/index.jsx
+++ src/components/Section/index.jsx
@@ -1,15 +1,10 @@
-import PropTypes from 'prop-types';
 import { Title } from './Section.styled';
 
 export default function Section({ title, children }) {
   return (
-    <div>
+    <section>
       <Title>{title}</Title>
       {children}
-    </div>
+    </section>
   );
 }
-
-Section.propTypes = {
-  title: PropTypes.string.isRequired,
-};
