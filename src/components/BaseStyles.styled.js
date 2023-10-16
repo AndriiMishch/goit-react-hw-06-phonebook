c93acdd0a7cf14bf8e589714bcf867cc3f8f531b
AndriiMishch <anderito02@gmail.com>
2023-10-16 21:21:08 +0200

    Update BaseStyles.styled.js

--- src/components/BaseStyles.styled.js
+++ src/components/BaseStyles.styled.js
@@ -1,39 +1,33 @@
-import styled, { css } from 'styled-components';
+import { createGlobalStyle } from 'styled-components';
 
-export const Button = styled.button`
-  background: transparent;
-  border-radius: 3px;
-  border: 2px solid #9b9ea3;
-  color: #9b9ea3;
-  padding: 0.25em 1em;
-  transition: all 500ms cubic-bezier(0.075, 0.82, 0.165, 1);
-
-  &:hover,
-  &:focus {
-    background: #9b9ea3;
-    color: white;
-  }
-
-  ${props =>
-    props.$primary &&
-    css`
+export const GlobalStyle = createGlobalStyle`
+    body{
       background: #9b9ea3;
-      color: white;
 
-      &:hover,
-      &:focus {
-        background: transparent;
-        color: #9b9ea3;
+
+      display: flex;
+      justify-content: center;
+    }
+    button {
+      background: transparent;
+      border-radius: 3px;
+      border: 2px solid #9b9ea3;
+      color: #9b9ea3;
+      padding: 0.25em 1em;
+      transition: all 500ms cubic-bezier(0.075, 0.82, 0.165, 1);
+    
+      &:focus, &:hover {
+        background: #9b9ea3;
+        color: white;
       }
-    `}
-`;
-
-export const Input = styled.input`
-  background: transparent;
-  border-radius: 3px;
-  border: 2px solid #9b9ea3;
-  color: #9b9ea3;
-  padding: 0.25em 1em;
-  width: 155px;
-  transition: all 500ms cubic-bezier(0.075, 0.82, 0.165, 1);
+    }
+    input{
+      background: transparent;
+      border-radius: 3px;
+      border: 2px solid #9b9ea3;
+      color: #9b9ea3;
+      padding: 0.25em 1em;
+      width: 155px;
+      transition: all 500ms cubic-bezier(0.075, 0.82, 0.165, 1);
+    }
 `;
